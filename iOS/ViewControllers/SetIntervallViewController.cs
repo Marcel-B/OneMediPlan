7d94260a5af946ccb2a10540fa405be50e94d851
marcel@macBook <marcel.benders@icloud.com>
2018-07-29 15:35:49 +0200

    Intervall page

---
+++ iOS/ViewControllers/SetIntervallViewController.cs
@@ -0,0 +1,76 @@
+using System;
+using UIKit;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OneMediPlan.iOS
+{
+    public partial class SetIntervallViewController : UIViewController
+    {
+        IEnumerable<string> IntervallTypes =
+            new[] { "Minute(n)", "Stunde(n)", "Tag(e)", "Woche(n)" };
+        public SetIntervallViewController(IntPtr handle) : base(handle) { }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var pickerModle = new IntervallTypeDataModel();
+            pickerModle.Items.AddRange(IntervallTypes.ToList());
+            PickerIntervallType.Model = pickerModle;
+        }
+
+        public class IntervallTypeDataModel : UIPickerViewModel
+        {
+            public event EventHandler<EventArgs> ValueChanged;
+
+            /// <summary>
+            /// The items to show up in the picker
+            /// </summary>
+            public List<string> Items { get; private set; }
+
+            /// <summary>
+            /// The current selected item
+            /// </summary>
+            public string SelectedItem
+            {
+                get => Items[selectedIndex];
+            }
+
+            int selectedIndex = 0;
+
+            public IntervallTypeDataModel()
+            {
+                Items = new List<string>();
+            }
+
+            /// <summary>
+            /// Called by the picker to determine how many rows are in a given spinner item
+            /// </summary>
+            public override nint GetRowsInComponent(UIPickerView pickerView, nint component)
+                => Items.Count;
+
+
+            /// <summary>
+            /// called by the picker to get the text for a particular row in a particular
+            /// spinner item
+            /// </summary>
+            public override string GetTitle(UIPickerView pickerView, nint row, nint component)
+                => Items[(int)row];
+
+            /// <summary>
+            /// called by the picker to get the number of spinner items
+            /// </summary>
+            public override nint GetComponentCount(UIPickerView pickerView)
+                => 1;
+
+            /// <summary>
+            /// called when a row is selected in the spinner
+            /// </summary>
+            public override void Selected(UIPickerView pickerView, nint row, nint component)
+            {
+                selectedIndex = (int)row;
+                ValueChanged?.Invoke(this, new EventArgs());
+            }
+        }
+    }
+}
---
+++ iOS/ViewControllers/SetIntervallViewController.designer.cs
@@ -0,0 +1,38 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace OneMediPlan.iOS
+{
+    [Register ("SetIntervallViewController")]
+    partial class SetIntervallViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField LabelIntervallCount { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIPickerView PickerIntervallType { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (LabelIntervallCount != null) {
+                LabelIntervallCount.Dispose ();
+                LabelIntervallCount = null;
+            }
+
+            if (PickerIntervallType != null) {
+                PickerIntervallType.Dispose ();
+                PickerIntervallType = null;
+            }
+        }
+    }
+}
